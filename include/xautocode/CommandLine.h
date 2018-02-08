35fba548ea3709c097fbac138c38c2e046637a80
lx <hypeaches@live.cn>
2018-02-08 09:08:11 +0800

    命令行参数增加--newline和--left-brace-pos选项

--- include/xautocode/CommandLine.h
+++ include/xautocode/CommandLine.h
@@ -5,9 +5,16 @@ class CommandLine
 {
 public:
     static void Parse(int argc, char* argv[]);
+    /// 头文件名或头文件目录
     static const char* header;
+    /// 源文件名或源文件目录
     static const char* source;
+    /// 源文件后缀名，默认值：cpp
     static const char* ext;
+    /// 换行符。Unix格式换行符LF(\n)或Windows格式换行符CRLF(\r\n)。默认值：LF
+    static const char* newline;
+    /// 左花括号的位置。与函数签名在同一行(0)或另起一行(1)。默认值：0
+    static int   left_brace_pos;
 };
 
 #endif // XAUTOCODE_COMMANDLINE_H
--- src/CommandLine.cpp
+++ src/CommandLine.cpp
@@ -5,15 +5,19 @@
 #include <stdexcept>
 #include <boost/program_options.hpp>
 
-const char* CommandLine::header = nullptr;
-const char* CommandLine::source = nullptr;
-const char* CommandLine::ext    = nullptr;
+const char* CommandLine::header         = nullptr;
+const char* CommandLine::source         = nullptr;
+const char* CommandLine::ext            = nullptr;
+const char* CommandLine::newline        = nullptr;
+int         CommandLine::left_brace_pos = 0;
 
 namespace {
     boost::program_options::options_description desc("Options");
-    std::string cmd_header;
-    std::string cmd_source;
-    std::string cmd_ext;
+    std::string cmd_header("");
+    std::string cmd_source("");
+    std::string cmd_ext("");
+    std::string cmd_newline("\n");
+    int         cmd_left_brace_pos = 0;
 }
 
 void InitCommandLine();
@@ -24,7 +28,9 @@ void CommandLine::Parse(int argc, char* argv[])
         ("help", "帮助")
         ("header", boost::program_options::value<std::string>(), "指定头文件")
         ("source", boost::program_options::value<std::string>(), "指定源文件")
-        ("ext", boost::program_options::value<std::string>(), "源文件后缀名，默认值为cpp");
+        ("ext", boost::program_options::value<std::string>(), "源文件后缀名，默认值：cpp")
+        ("newline",boost::program_options::value<std::string>(), "换行符LF(\n)或CRFL(\r\n)，默认值：LF")
+        ("left-brace-pos",boost::program_options::value<int>(), "左花括号位置，与函数签名在同一行(0)或另起一行(1)，默认值：0");
 
     boost::program_options::variables_map vm;
     boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
@@ -48,6 +54,14 @@ void CommandLine::Parse(int argc, char* argv[])
     {
         cmd_ext = vm["ext"].as<std::string>();
     }
+    if (vm.count("newline"))
+    {
+        cmd_newline = vm["newline"].as<std::string>();
+    }
+    if (vm.count("left-brace-pos"))
+    {
+        cmd_left_brace_pos = vm["left-brace-pos"].as<int>();
+    }
 
     InitCommandLine();
 }
@@ -57,4 +71,6 @@ void  InitCommandLine()
     CommandLine::header = cmd_header.c_str();
     CommandLine::source = cmd_source.c_str();
     CommandLine::ext = cmd_ext.c_str();
+    CommandLine::newline = cmd_newline.c_str();
+    CommandLine::left_brace_pos = cmd_left_brace_pos;
 }
