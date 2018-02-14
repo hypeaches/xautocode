ea4fd6ad1d74e50c2933e5e61a6da87ad840dae8
hypeaches <hypeaches@live.cn>
2018-02-14 12:51:36 +0800

    设计头文件解析和源文件输出

--- include/xautocode/Generator/HeaderFile.h
+++ include/xautocode/Generator/HeaderFile.h
@@ -1,9 +1,36 @@
 #ifndef XAUTOCODE_GENERATOR_HEADERFILE_H
 #define XAUTOCODE_GENERATOR_HEADERFILE_H
 
+#include <cstdio>
+
 class HeaderFile
 {
+public:
+    char* return_type;
+    char* function;
+    char* name_space;
+    char* class_name;
+    void Open(const char* file_name);
+    void Close();
+    char* ParseNamespace();
+    char* ParseClassName();
+    bool ReadLine();
 
+private:
+    bool DoReadLine();
+    bool IsFunction();
+    void Trim();
+    void TrimLeft();
+    void TrimRight();
+    FILE* _file;
+    char* ParseReturnType();
+    char* ParseFunction();
+    char* _head;
+    int _line_length;
+    char* _buffer[4 * 1024];
+    char _class_name[32];
+    char _name_space[32];
+    char _name_space_end[32];
 };
 
 #endif // XAUTOCODE_GENERATOR_HEADERFILE_H
--- include/xautocode/Generator/SourceFile.h
+++ include/xautocode/Generator/SourceFile.h
@@ -1,9 +1,21 @@
 #ifndef XAUTOCODE_GENERATOR_SOURCEFILE_H
 #define XAUTOCODE_GENERATOR_SOURCEFILE_H
 
+#include <cstdio>
+
 class SourceFile
 {
+public:
+    FILE* file()
+    {
+        return _file;
+    }
+    bool Exist(const char* file_name);
+    bool Open(const char* file_name);
+    void Close();
 
+private:
+    FILE* _file;
 };
 
 #endif // XAUTOCODE_GENERATOR_SOURCEFILE_H
