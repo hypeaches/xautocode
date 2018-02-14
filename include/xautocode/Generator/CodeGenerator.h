07d55254e5b1c949d21c4815de3ce80c012eadd7
hypeaches <hypeaches@live.cn>
2018-02-14 12:56:24 +0800

    设计代码生成类

--- include/xautocode/Generator/CodeGenerator.h
+++ include/xautocode/Generator/CodeGenerator.h
@@ -1,9 +1,19 @@
 #ifndef XAUTOCODE_GENERATOR_CODEGENERATOR_H
 #define XAUTOCODE_GENERATOR_CODEGENERATOR_H
 
+class HeaderFile;
+class SourceFile;
+
 class CodeGenerator
 {
+public:
+    CodeGenerator();
+    ~CodeGenerator();
+    void Work(const char* header_file_name, const char* source_file_name);
 
+private:
+    HeaderFile* _header_file;
+    SourceFile* _source_file;
 };
 
 #endif // XAUTOCODE_GENERATOR_CODEGENERATOR_H
